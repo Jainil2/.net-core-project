41a17ae9adb51f158197c907e4f5367380b3add9
Jainil <chauhanjainil305@gmail.com>
2023-10-25 03:00:42 +0530

    first commit

---
+++ JobPortalMVC/Controllers/UserController.cs
@@ -0,0 +1,137 @@
+﻿using DatabaseLayer;
+using JobPortalMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPortalMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private JobshuntDbEntities db = new JobshuntDbEntities();
+        // GET: User
+        public ActionResult NewUser()
+        {
+            return View(new UserMV());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NewUser(UserMV userMV)
+        {
+            if(ModelState.IsValid)
+            {
+                var checkuser = db.UserTables.Where(u => u.EmailAddress == userMV.EmailAddress).FirstOrDefault();
+                if(checkuser != null)
+                {
+                    ModelState.AddModelError("EmailAddress", "Email is already register");
+                    return View(userMV);
+                }
+                checkuser = db.UserTables.Where(u => u.UserName == userMV.UserName).FirstOrDefault();
+                if (checkuser != null)
+                {
+                    ModelState.AddModelError("UserName", "UserName is already register");
+                    return View(userMV);
+                }
+                using (var trans = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var user = new UserTable();
+                        user.UserName = userMV.UserName;
+                        user.Password = userMV.Password;
+                        user.ContactNo = userMV.ContactNo;
+                        user.EmailAddress = userMV.EmailAddress;
+                        user.UserTypeID = userMV.AreYouProvider == true ? 2 : 3;
+                        db.UserTables.Add(user);
+                        db.SaveChanges();
+                        if (userMV.AreYouProvider == true)
+                        {
+                            var company = new CompanyTable();
+                            company.UserID = user.UserID;
+                            if(string.IsNullOrEmpty(userMV.Company.EmailAddress))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.EmailAddress", "Required*");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.CompanyName))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.CompanyName", "Required*");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.PhoneNo))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.PhoneNo", "Required*");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.Description))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.Description", "Required*");
+                                return View(userMV);
+                            }
+                            company.EmailAddress = userMV.Company.EmailAddress;
+                            company.CompanyName = userMV.Company.CompanyName;
+                            company.ContactNo = userMV.ContactNo;
+                            company.PhoneNo = userMV.Company.PhoneNo;
+                            company.Logo = "~/Content/assets/img/logo/logo.png";
+                            company.Description = userMV.Company.Description;
+                            db.CompanyTables.Add(company);
+                            db.SaveChanges();
+                        }
+                        trans.Commit();
+                        return RedirectToAction("Login");
+                    }
+                    catch (Exception)
+                    {
+                        ModelState.AddModelError(String.Empty, "Please provide correct details");
+                        trans.Rollback();
+                    }
+                    
+                }
+            }
+            return View(userMV);
+        }
+        public ActionResult Login()
+        {
+            return View(new UserLoginMV());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLoginMV userLoginMV)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = db.UserTables.Where(u=>u.UserName == userLoginMV.UserName && u.Password == userLoginMV.Password).FirstOrDefault();
+                if(user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Please Provide Correct Details");
+                    return View(userLoginMV);
+                }
+                Session["UserID"] = user.UserID;
+                Session["UserName"] = user.UserName;
+                Session["UserTypeID"] = user.UserTypeID;
+                if(user.UserTypeID == 2)
+                {
+                    Session["CompanyID"] = user.CompanyTables.FirstOrDefault().CompanyID;
+                }
+                return RedirectToAction("Index","Home");
+            }
+            return View(userLoginMV);
+        }
+        public ActionResult Logout()
+        {
+            Session["UserID"] = string.Empty;
+            Session["UserName"] = string.Empty;
+            Session["CompanyID"] = string.Empty;
+            Session["UserTypeID"] = string.Empty;
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
